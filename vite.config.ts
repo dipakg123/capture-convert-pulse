1d06574d21fb709087fb574c81591abd6530db8a
dipakg123 <gdipakh@gmail.com>
2025-06-11 14:44:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/      
 export default defineConfig(({ mode }) => ({
-  base: '/capture-convert-pulse/'
+  base: '/capture-convert-pulse/',
   server: {
     host: "::",
     port: 8080,
